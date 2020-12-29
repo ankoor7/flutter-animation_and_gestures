a6542cc54c6eef65c225f8f7481ac4579c3e477a
Ankoor Patel <ankoor.p@gmail.com>
2020-12-29 11:40:40 +0000

    Pg. 155 finished chapter

--- lib/pong.dart
+++ lib/pong.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:simple_pong/ball.dart';
 import 'package:simple_pong/bat.dart';
@@ -16,11 +18,16 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   double batHeight = 0;
   double batPosition = 0;
   double increment = 5;
+  static final Random random = Random();
+  double xMultiplier = 1;
+  double yMultiplier = 1;
+  int score = 0;
 
   Ball ball = Ball();
 
   Animation<double> animation;
   AnimationController controller;
+  double tweenMaxValue = 100;
 
   @override
   void initState() {
@@ -30,11 +37,15 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
       duration: Duration(minutes: 10000),
       vsync: this,
     );
-    animation = Tween<double>(begin: 0, end: 100).animate(controller);
+    animation = Tween<double>(begin: 0, end: tweenMaxValue).animate(controller);
     animation.addListener(() {
       safeSetState(() {
-        (hDir == Direction.right) ? posX += increment : posX -= increment;
-        (vDir == Direction.down) ? posY += increment : posY -= increment;
+        (hDir == Direction.right)
+            ? posX += (increment + xMultiplier).round()
+            : posX -= (increment + xMultiplier).round();
+        (vDir == Direction.down)
+            ? posY += (increment + yMultiplier).round()
+            : posY -= (increment + yMultiplier).round();
       });
       checkBorders();
     });
@@ -61,6 +72,11 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
 
         return Stack(
           children: <Widget>[
+            Positioned(
+              child: Text("Score: $score"),
+              top: 0,
+              right: 24,
+            ),
             Positioned(
               child: ball,
               left: posX,
@@ -92,22 +108,63 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   void checkBorders() {
     if (posX <= 0 && hDir == Direction.left) {
       hDir = Direction.right;
+      xMultiplier = randomNumber();
     }
     if (width != null && posX >= width - ball.diameter && hDir == Direction.right) {
       hDir = Direction.left;
+      xMultiplier = randomNumber();
     }
     if (height != null && posY >= height - ball.diameter - batHeight && vDir == Direction.down) {
       // check if bat is under the ball
       if (posX >= (batPosition - ball.diameter) && posX <= (batPosition + batWidth + ball.diameter)) {
         vDir = Direction.up;
+        yMultiplier = randomNumber();
+        safeSetState(() {
+          score++;
+        });
       } else {
         controller.stop();
-        dispose();
+        showMessage(context);
       }
     }
     if (posY <= 0 && vDir == Direction.up) {
       vDir = Direction.down;
+      yMultiplier = randomNumber();
     }
+  }
+
+  double randomNumber() => ((tweenMaxValue.toInt() / 2) + random.nextInt(tweenMaxValue.toInt() + 1)) / tweenMaxValue;
+
+  void showMessage(BuildContext context) {
+    showDialog(context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Game Over'),
+          content: Text('Would you like to play again?'),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                setState(() {
+                  posX = 0;
+                  posY = 0;
+                  score = 0;
+                });
+                Navigator.of(context).pop();
+                controller.repeat();
+              },
+              child: Text('Yes'),
+            ),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  dispose();
+                },
+                child: Text('No'),
+            )
+          ],
+        );
+      }
+    );
   }
 
   void safeSetState(Function function) {
